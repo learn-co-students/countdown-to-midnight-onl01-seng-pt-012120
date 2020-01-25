3105f0bd6f3dd7f5e92caa29964ac24b6ea7afb7
Beth Ruff <beth.ruff2@gmail.com>
2020-01-25 21:11:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
-
-def countdown
+def countdown(n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+  
+def countdown_with_sleep(n)
+  n = 5
+  until n == 0
+    sleep 1
+    n -= 1
+  end    
 end
