57487247d8fe68da2753b10301848d02653fdc83
Dominc Carolan <dominiccarolan@gmail.com>
2020-01-26 17:33:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
+nye_countdown = 10
+
 def countdown
+  while nye_countdown > 0
+    nye_countdown -= 1
+    puts "#{nye_countdown} SECOND(S)!"
+end
+
+    "HAPPY NEW YEAR!"
 end
