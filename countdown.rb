7dade88ad701c9407e88aa4c316a9d75bff78f38
antromo <anthonyroma741@gmail.com>
2020-01-26 23:33:28 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(secs)
+    until secs == 0
+        puts "#{secs} SECOND(S)!"
+        secs -= 1
 
-def countdown
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(secs)
+    sleep (5)
+while secs > 0
+    puts "#{secs} SECOND(S)"
+    secs -= 1
+end
+"HAPPY NEW YEAR!"
 end
