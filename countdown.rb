5b93b84bad3eb2e74e5ebd7efc7de440024a7127
Chance cline <clinechance@gmail.com>
2020-01-27 19:24:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
 
-def countdown
+
+def countdown(secs)
+  until secs == 0 
+  puts "#{secs}SECOND(S)! " 
+  secs -=1
 end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(secs)
+  sleep(5)
+  while secs > 0 
+  puts  "#{secs}SECOND(S) " 
+  secs -= 1 
+end
+"HAPPY NEW YEAR!"
+end
+
